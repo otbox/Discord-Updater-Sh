fc5a4e37c736ada49cdd85503247c24a84713766
Otávio Cruz <otboxmarques88@gmail.com>
2025-01-14 22:30:19 -0300

    Update updateDiscord.sh

--- updateDiscord.sh
+++ updateDiscord.sh
@@ -1,34 +1,33 @@
 #!/bin/bash
 
-# Caminho para o arquivo JSON em /opt
+# path for the Discord JSON
 file="/opt/discord/resources/build_info.json"
 
 # Verifica se o arquivo existe
 if [ ! -f "$file" ]; then
-  echo "Arquivo $file não encontrado!"
+  echo "file $file not found!"
   exit 1
 fi
 
 # Verifica se o script está sendo executado como root
 if [ "$EUID" -ne 0 ]; then
-  echo "Por favor, execute o script com sudo."
+  echo "Please run with sudo"
   exit 1
 fi
-
-# Lê a versão atual do arquivo JSON
+# Read the discord version
 version=$(grep -oP '(?<="version": ")[^"]*' $file)
 
-# Divide a versão em componentes (major, minor, patch)
+# Divide the version in variables (major, minor, patch)
 IFS='.' read -r major minor patch <<< "$version"
 
-# Incrementa o número de versão (ajustável: major, minor ou patch)
+# Increase the number of version (adjustable: major, minor ou patch)
 minor=0
-patch=$((patch + 1)) # Reinicia o patch
+patch=$((patch + 1)) 
 
-# Nova versão formatada
+# New version formatted 
 new_version="$major.$minor.$patch"
 
-# Substitui a versão no arquivo JSON (usando sudo)
+# Replace the version (with sudo)
 sed -i "s/\"version\": \"$version\"/\"version\": \"$new_version\"/" $file
 
-echo "Versão atualizada de $version para $new_version"
+echo "Updated the $version to $new_version"
